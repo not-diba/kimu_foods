f6a81a223b7a279cbe8b6109e8d766d2bd20d62a
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2023-12-01 01:37:44 +0300

    components

---
+++ lib/components/elevated_primary_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kimu_foods/utils/utils.dart';
+
+class ElevatedPrimaryButton extends StatefulWidget {
+  final double amount;
+  final String label;
+  const ElevatedPrimaryButton(
+      {super.key, required this.amount, required this.label});
+
+  @override
+  State<ElevatedPrimaryButton> createState() => _ElevatedPrimaryButtonState();
+}
+
+class _ElevatedPrimaryButtonState extends State<ElevatedPrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24, right: 24),
+      child: SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            elevation: 8,
+            backgroundColor: mainColor[500],
+            foregroundColor: Colors.white,
+            padding:
+                const EdgeInsets.only(top: 16, bottom: 16),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+            textStyle: GoogleFonts.rubik(
+              fontSize: 18,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          onPressed: () {},
+          child: Text('${widget.label} • ${formatAmount(widget.amount)}'),
+        ),
+      ),
+    );
+  }
+}
